00f7066032030bf62515afd71661541b38342008
SXK <61637558+Snipexkillo@users.noreply.github.com>
2022-12-03 16:01:25 -0600

    Create Anmol.java

---
+++ AdventOfCode2022/Day1/A/Anmol.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.*;
+public class workspace4 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int largest= 0;
+		int currsum = 0;
+		String curinput;
+		
+		while(true) {
+			curinput = input.nextLine();
+			if(curinput.equals("no")) {
+				if(largest<currsum) {
+					largest = currsum;
+				}
+				currsum = 0;
+				break;
+			}
+			if(curinput.isEmpty()) {
+				if(largest<currsum) {
+					largest = currsum;
+				}
+				currsum = 0;
+			}else {
+				currsum += Integer.parseInt(curinput);
+			}
+		}
+		System.out.println(largest);
+	}
+}
