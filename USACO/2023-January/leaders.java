9ac108bf4c8bb1957dccabb632ca61ee0bcb6c89
SXK <61637558+Snipexkillo@users.noreply.github.com>
2023-02-08 12:53:42 -0600

    Add files via upload

---
+++ USACO/2023-January/leaders.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    static int lowestH;
+    static int lowestG;
+    static int highestH;
+    static int highestG;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine();
+        String ord = input.nextLine();
+        int[] arr = new int[n];
+        for (int i = 0; i<n; i++){
+            arr[i] = input.nextInt();
+        }
+        lowestH = -1;
+        lowestG = -1;
+        highestH = n;
+        highestG = n;
+        boundFinder(ord,n);
+        //System.out.println(lowestH + " " + highestH);
+        //System.out.println(lowestG + " " + highestG);
+        ArrayList<Integer> validH = new ArrayList<Integer>();
+        ArrayList<Integer> validG = new ArrayList<Integer>();
+        pureLeaders(validG, false, ord, arr);
+        pureLeaders(validH, true, ord, arr);
+        int count = validH.size() * validG.size();
+        //System.out.println(validG.size() + " " + validH.size());
+        for(int i = 0; i<validG.size(); i++){
+            for(int a = 0; a<validG.get(i); a++){
+                if(ord.charAt(a) == 'H'){
+                    if(arr[a]-1>=validG.get(i)){
+                        count++;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i<validH.size(); i++){
+            for(int a = 0; a<validH.get(i); a++){
+                if(ord.charAt(i) == 'G'){
+                    if(arr[a]-1>=validH.get(i)){
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+        
+        
+        
+    }
+    public static void boundFinder(String str, int n){
+        int i = n-1;
+        while(highestH == n){
+           if(str.charAt(i) == 'H'){
+               highestH = i;
+           } 
+           i--;
+        }
+        i = n-1;
+        while(highestG == n){
+           if(str.charAt(i) == 'G'){
+               highestG = i;
+           } 
+           i--;
+        }
+        i = 0;
+        while(lowestH == -1){
+           if(str.charAt(i) == 'H'){
+               lowestH = i;
+           } 
+           i++;
+        }
+        i = 0;
+        while(lowestG == -1){
+           if(str.charAt(i) == 'G'){
+               lowestG = i;
+           } 
+           i++;
+        }
+    }
+    public static void pureLeaders(ArrayList<Integer> loc, boolean isH, String str, int[] arr){
+        if(isH){
+            for(int i = 0; i<=lowestH;i++){
+                if(str.charAt(i) == 'H' && arr[i]-1>=highestH){
+                    loc.add(i);
+                }
+            }
+        }else{
+            for(int i = 0; i<=lowestG;i++){
+                if(str.charAt(i) == 'G' && arr[i]-1>=highestG){
+                    loc.add(i);
+                }
+            }
+        }
+    }
+}
